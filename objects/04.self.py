16eaae3bcb635b98674b40fadd192097c2811d9a
aylin <a.sanlazo@gmail.com>
2022-09-26 10:54:05 -0300

    self

--- objects/04.self.py
+++ objects/04.self.py
@@ -3,3 +3,20 @@
 # palabra reservada class
 # 
 
+
+class Student:
+    def __init__(self,name,position,skills):
+        self.name = name
+        self.position = position
+        self.skills = skills 
+    
+    def say_name(self):
+        print("Mi nombre es",self.name,"Mi cargo es",self.position,"y mis habilidades son",self.skills)
+
+Alice = Student("alice","Fullstack Developer",["Python", "Git", "HTML","CSS","Javascript"])
+Bob_el_chef = Student("bob el chef","kitchen assistant",["international cuisine","pastry"])
+Alice.say_name()
+Bob_el_chef.say_name()
+
+
+
